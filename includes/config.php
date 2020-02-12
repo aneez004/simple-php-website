5303ecf659e0aa534df422591a22a203166fd3ff
aneez004 <aneez004@gmail.com>
2020-02-12 11:36:37 +0530

    hello5

--- includes/config.php
+++ includes/config.php
@@ -9,7 +9,7 @@ function config($key = '')
 {
     $config = [
         'name' => 'Simple PHP Website',
-        'site_url' => 'phpsite-pro1.127.0.0.1.nip.io',
+        'site_url' => 'gitapp-sydney.127.0.0.1.nip.io',
         'pretty_uri' => true,
         'nav_menu' => [
             '' => 'Home',
